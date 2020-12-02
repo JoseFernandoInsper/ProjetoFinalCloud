923d2a872715178016a14197624a42d023b0e535
Jose Fernando de Melo Cruz <josefmc@al.insper.edu.br>
2020-12-02 02:19:02 -0300

    ORM funcionando

--- newinstaceBoto3.py
+++ newinstaceBoto3.py
@@ -1,6 +1,7 @@
+import sys
 import boto3
 from decouple import config
-#from botocore.exceptions import ClientError
+from botocore.exceptions import ClientError
 
 AWS_ACCESS_KEY_ID = config('aws_access_key_id')
 AWS_SECRET_ACCESS_KEY = config('aws_secret_access_key')
@@ -14,68 +15,148 @@ ec2resource_ohio = session.resource('ec2', region_name='us-east-2')
 ec2client_virginia = session.client('ec2', region_name='us-east-1')
 ec2resource_virginia = session.resource('ec2', region_name='us-east-1')
 
-ec2LoadBclient_virginia= session.client('ec2', region_name= 'us-east-1')
-ec2AutoSclient_virginia= session.client('ec2', region_name= 'us-east-1')
+##ec2LoadBclient_virginia= session.client('ec2', region_name= 'us-east-1')
+##ec2AutoSclient_virginia= session.client('ec2', region_name= 'us-east-1')
 
-userData= """#!/bin/sh
-    cd home/ubuntu
-    sudo apt update
-    git clone https://github.com/JoseFernandoInsper/ProjetoFinalCloud.git
-    cd ProjetoFinalCloud
-    chmod +x criar_bd.sh
-    ./criar_bd.sh
-    """
-create_instanceOhio = ec2resource_ohio.create_instances(
-  ImageId='ami-0dd9f0e7df0f0a138',
-  InstanceType = 't2.micro',
-  MaxCount=1,
-  MinCount=1,
-  TagSpecifications=[
-        {
-            'ResourceType': 'instance',
-            'Tags': [
-                {
-                    'Key': 'Name',
-                    'Value': 'instDB'
-                },
-            ]
-        },
-    ],
-  SecurityGroupIds=['sg-04f1b35ff4633d922'],
-  SecurityGroups=['securityJose'],
-  KeyName='josekeys',
-  UserData=userData
-)
 
-Virginia=  """#!/bin/sh
-    cd home/ubuntu
-    sudo apt update
-    git clone https://github.com/raulikeda/tasks.git
-    sudo sed -i 's/node1/{0}/' /home/ubuntu/tasks/portfolio/settings.py
-    cd tasks
-    ./install.sh
-    cd ..
-    sudo reboot
-    """
+def definirIp(cliente, nome):
+  instanceIp = cliente.describe_instances(
+    Filters=[
+      {
+        'Name': 'tag:Name',
+        'Values': [nome]
+      },
+      {
+        'Name': 'instance-state-name',
+        'Values': ['running']
+      }
+    ]
+  )
+  ip = instanceIp['Reservations'][0]['Instances'][0]['PublicIpAddress']
+  return(ip)
 
-create_instanceVirginia = ec2resource_virginia.create_instances(
-  ImageId = 'ami-00ddb0e5626798373',
-  InstanceType = 't2.micro',
-  MaxCount=1,
-  MinCount=1,
-  TagSpecifications=[
-        {
-            'ResourceType': 'instance',
-            'Tags': [
-                {
-                    'Key': 'Name',
-                    'Value': 'JoseVirginia'
-                },
-            ]
-        },
-    ],
-  SecurityGroupIds=['sg-02b2377c79fdee256'],
-  SecurityGroups=['securityJose'],
-  KeyName='josekeys',
-  UserData=Virginia
-)
+def definirId(cliente, nome):
+  instanceId = cliente.describe_instances(
+    Filters=[
+      {
+        'Name': 'tag:Name',
+        'Values': [nome]
+      },
+      {
+        'Name': 'instance-state-name',
+        'Values': ['running']
+      }
+
+    ]
+  )
+  id = instanceId['Reservations'][0]['Instances'][0]['InstanceId']
+  return(id)
+
+def delete_instancia(cliente, nome):
+  instance_id = cliente.describe_instances(
+    Filters=[
+      {
+        'Name': 'tag:Name',
+        'Values': [nome]
+      }
+    ]
+  )
+  ids = []
+  for reservation in (instance_id["Reservations"]):
+    for instance in reservation["Instances"]:
+      ids.append(instance['InstanceId'])
+  
+  try:
+    if len(ids)>0:
+      cliente.terminate_instances(InstanceIds=ids)
+      cliente.get_waiter('instance_terminated').wait(InstanceIds=ids)
+  except ClientError as e:
+    print(e)
+
+
+    
+def ohio():
+  userData= """#!/bin/sh
+      cd home/ubuntu
+      sudo apt update
+      git clone https://github.com/JoseFernandoInsper/ProjetoFinalCloud.git
+      cd ProjetoFinalCloud
+      chmod +x criar_bd.sh
+      ./criar_bd.sh
+      """
+  try:
+    create_instanceOhio = ec2resource_ohio.create_instances(
+      ImageId='ami-0dd9f0e7df0f0a138',
+      InstanceType = 't2.micro',
+      MaxCount=1,
+      MinCount=1,
+      TagSpecifications=[
+            {
+                'ResourceType': 'instance',
+                'Tags': [
+                    {
+                        'Key': 'Name',
+                        'Value': 'instDB'
+                    },
+                ]
+            },
+        ],
+      SecurityGroupIds=['sg-04f1b35ff4633d922'],
+      SecurityGroups=['securityJose'],
+      KeyName='josekeys',
+      UserData=userData
+    )
+    instanceId = 0
+    ec2client_ohio.get_waiter('instance_status_ok').wait(InstanceIds=[create_instanceOhio[0].id])
+    print('passoi')
+  except ClientError as e:
+    print(e)
+
+
+def VirginiaInst(ohioIp):
+  Virginia=  """#!/bin/sh
+      cd home/ubuntu
+      sudo apt update
+      git clone https://github.com/raulikeda/tasks.git
+      sudo sed -i 's/node1/{0}/' /home/ubuntu/tasks/portfolio/settings.py
+      cd tasks
+      ./install.sh
+      cd ..
+      sudo reboot
+      """.format(ohioIp)
+  try:
+    create_instanceVirginia = ec2resource_virginia.create_instances(
+      ImageId = 'ami-00ddb0e5626798373',
+      InstanceType = 't2.micro',
+      MaxCount=1,
+      MinCount=1,
+      TagSpecifications=[
+            {
+                'ResourceType': 'instance',
+                'Tags': [
+                    {
+                        'Key': 'Name',
+                        'Value': 'JoseVirginia'
+                    },
+                ]
+            },
+        ],
+      SecurityGroupIds=['sg-02b2377c79fdee256'],
+      SecurityGroups=['securityJose'],
+      KeyName='josekeysNV',
+      UserData=Virginia
+    )
+    ec2client_virginia.get_waiter('instance_status_ok').wait(InstanceIds=[create_instanceVirginia[0].id])
+    print("pasosu")
+  except ClientError as e:
+    print(e)
+
+delete_instancia(ec2client_ohio, 'instDB')
+delete_instancia(ec2client_virginia, 'JoseVirginia')
+
+ohio()
+ohioip = definirIp(ec2client_ohio, 'instDB')
+
+VirginiaInst(ohioip)
+
+
